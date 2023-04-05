3da9441f4494121ec815ca372eed4227397bb740
Lars Kiesow <lkiesow@uos.de>
2023-04-05 19:20:11 +0200

    Release 0.4

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-'''
-Copyright 2022, ELAN e.V. <kontakt-elan@elan-ev.de>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-'''
-
 from setuptools import setup
 import os
 
@@ -28,7 +12,7 @@ def read(filename):
 
 setup(
     name='proteuscmd',
-    version='0.3',
+    version='0.4',
     description=description_text,
     url='https://github.com/virtUOS/proteuscmd',
     author='Lars Kiesow',
