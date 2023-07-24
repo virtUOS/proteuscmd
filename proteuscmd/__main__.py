e35a9eaa1e189e70146f899bc05ca79bc6d9b922
Lars Kiesow <lkiesow@uos.de>
2023-07-24 21:39:46 +0200

    Separate API methods

--- proteuscmd/__main__.py
+++ proteuscmd/__main__.py
@@ -1,8 +1,8 @@
 import argparse
-import ipaddress
 import json
 import pathlib
-import requests
+
+from proteuscmd.api import Proteus
 
 __config = None
 
@@ -16,148 +16,6 @@ def config(key):
         with open(pathlib.Path().home() / '.proteus.json', 'r') as f:
             __config = json.load(f)
     return __config.get(key) if key else __config
-
-
-def url(path):
-    '''Build URL based on configuration.
-    '''
-    base_url = config('url')
-    path = path.lstrip('/')
-    return f'{base_url}/Services/REST/v1/{path}'
-
-
-def login():
-    '''Logging in at Proteus.
-    '''
-    payload = {'username': config('user'), 'password': config('password')}
-    result = requests.get(url('login'), params=payload).json()
-    token = result.split()[2] + ' ' + result.split()[3]
-    return {'Authorization': token, 'Content-Type': 'application/json'}
-
-
-def getEntitiesByName(auth_header, name, parent, object_type):
-    params = {'count': 10,
-              'name': name,
-              'parentId': parent,
-              'start': 0,
-              'type': object_type}
-    response = requests.get(url('getEntitiesByName'),
-                            params=params,
-                            headers=auth_header)
-    return response.json()
-
-
-def getEntities(auth_header, parent, object_type):
-    params = {'count': 50,
-              'parentId': parent,
-              'start': 0,
-              'type': object_type}
-    response = requests.get(url('getEntities'),
-                            params=params,
-                            headers=auth_header)
-    return response.json()
-
-
-def parse_domain(domain):
-    for src, to in (config('replace') or {}).items():
-        domain = domain.replace(src, to)
-    fragments = list(filter(bool, domain.split('.')[::-1]))
-    return fragments[:-1], fragments[-1]
-
-
-def get_requested_views(auth, view_arg):
-    # get configuration_id
-    data = getEntitiesByName(auth, 'default', 0, 'Configuration')
-    configuration_id = data[0]['id']
-
-    views = []
-
-    if view_arg in ('all', 'intern'):
-        data = getEntitiesByName(auth, 'intern', configuration_id, 'View')
-        views.append(('intern', data[0]['id']))
-
-    if view_arg in ('all', 'extern'):
-        data = getEntitiesByName(auth, 'extern', configuration_id, 'View')
-        views.append(('extern', data[0]['id']))
-
-    return views
-
-
-def record_type_from_target(target):
-    try:
-        ipaddress.ip_address(target)
-        return 'HostRecord'
-    except ValueError:
-        return 'AliasRecord'
-
-
-def get_record(auth, view, domain):
-
-    zones, host = parse_domain(domain)
-
-    # Navigate through zones
-    parent = view
-    for zone in zones:
-        data = getEntitiesByName(auth, zone, parent, 'Zone')
-        if not data:
-            zone_path = ' → '.join(zones)
-            print(f'Zone {zone_path} could not be found.')
-            exit(1)
-        parent = data[0]['id']
-
-    # Get host
-    data = getEntitiesByName(auth, host, parent, 'HostRecord') \
-        + getEntitiesByName(auth, host, parent, 'AliasRecord')
-    for record in data:
-        properties = record['properties'].split('|')
-        for prop in filter(bool, properties):
-            print(f'  {prop}')
-
-
-def set_record(auth, view, domain, target):
-
-    record_type = record_type_from_target(target)
-
-    if record_type == 'HostRecord':
-        params = {'absoluteName': domain,
-                  'addresses': target,
-                  'ttl': -1,
-                  'viewId': view}
-        response = requests.post(url('addHostRecord'),
-                                 params=params,
-                                 headers=auth)
-
-    else:  # add aliias record
-        params = {'absoluteName': domain,
-                  'linkedRecordName': target,
-                  'ttl': -1,
-                  'viewId': view}
-        response = requests.post(url('addAliasRecord'),
-                                 params=params,
-                                 headers=auth)
-
-    print(response.json())
-
-
-def delete_record(auth, view, domain):
-
-    zones, host = parse_domain(domain)
-
-    # Navigate through zones
-    parent = view
-    for zone in zones:
-        data = getEntitiesByName(auth, zone, parent, 'Zone')
-        parent = data[0]['id']
-
-    # Get host
-    data = getEntitiesByName(auth, host, parent, 'HostRecord') \
-        or getEntitiesByName(auth, host, parent, 'AliasRecord')
-    if not data:
-        return
-
-    payload = {'objectId': data[0]['id']}
-    result = requests.delete(url('delete'), params=payload, headers=auth)
-    print(result)
 
 
 def main():
@@ -181,20 +39,18 @@ def main():
         parser.error("set requires a target.")
 
     # Interaction with Proteus
-    auth_header = login()
-    views = get_requested_views(auth_header, args.view)
+    cfg = config('user'), config('password'), config('url'), config('replace')
+    with Proteus(*cfg) as proteus:
+        views = proteus.get_requested_views(args.view)
 
-    for name, view in views:
-        print(name)
-        if args.operation == 'get':
-            get_record(auth_header, view, args.domain)
-        elif args.operation == 'set':
-            set_record(auth_header, view, args.domain, args.target)
-        elif args.operation == 'delete':
-            delete_record(auth_header, view, args.domain)
-
-    # logout from BlueCat
-    requests.get(url('logout'), headers=auth_header)
+        for name, view in views:
+            print(name)
+            if args.operation == 'get':
+                print('\n'.join(proteus.get_record(view, args.domain)))
+            elif args.operation == 'set':
+                print(proteus.set_record(view, args.domain, args.target))
+            elif args.operation == 'delete':
+                print(proteus.delete_record(view, args.domain))
 
 
 if __name__ == '__main__':
---
+++ proteuscmd/api.py
@@ -0,0 +1,175 @@
+import ipaddress
+import requests
+
+
+class Proteus:
+
+    __user: str = ''
+    __password: str = ''
+    __base_url: str = ''
+    __replacements: dict[str, str] = {}
+    __auth_header: dict[str, str] = {}
+
+    def __init__(self, user, password, base_url, replacements):
+        self.__user = user
+        self.__password = password
+        self.__base_url = base_url
+        self.__replacements = replacements
+
+    def __enter__(self):
+        self.login()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.logout()
+
+    def __url(self, path):
+        '''Build URL based on configuration.
+        '''
+        path = path.lstrip('/')
+        return f'{self.__base_url}/Services/REST/v1/{path}'
+
+    def __post(self, path, params):
+        response = requests.post(self.__url(path),
+                                 params=params,
+                                 headers=self.__auth_header)
+        return response.json()
+
+    def __get(self, path, params):
+        response = requests.get(self.__url('getEntitiesByName'),
+                                params=params,
+                                headers=self.__auth_header)
+        return response.json()
+
+    def __delete(self, path, params):
+        return requests.delete(self.__url('delete'),
+                               params=params,
+                               headers=self.__auth_header)
+
+    def login(self):
+        '''Logging in at Proteus.
+        '''
+        payload = {'username': self.__user, 'password': self.__password}
+        result = requests.get(self.__url('login'), params=payload).json()
+        token = result.split()[2] + ' ' + result.split()[3]
+        self.__auth_header = {
+                'Authorization': token,
+                'Content-Type': 'application/json'}
+
+    def logout(self):
+        '''Logout from BlueCat
+        '''
+        self.__get('logout', {})
+
+    def get_entities_by_name(self, name, parent, object_type):
+        params = {'count': 10,
+                  'name': name,
+                  'parentId': parent,
+                  'start': 0,
+                  'type': object_type}
+        return self.__get('getEntitiesByName', params)
+
+    def get_entities(self, parent, object_type):
+        params = {'count': 50,
+                  'parentId': parent,
+                  'start': 0,
+                  'type': object_type}
+        return self.__get('getEntities', params)
+
+    def parse_domain(self, domain):
+        for src, to in self.__replacements.items():
+            domain = domain.replace(src, to)
+        fragments = list(filter(bool, domain.split('.')[::-1]))
+        return fragments[:-1], fragments[-1]
+
+    def get_requested_views(self, view_arg):
+        '''Get requested views.
+        view_arg can be intern, extern or all.
+        It controls if this will return the view intern, extern or both.
+        '''
+        # get configuration_id
+        data = self.get_entities_by_name('default', 0, 'Configuration')
+        conf_id = data[0]['id']
+
+        views = []
+
+        if view_arg in ('all', 'intern'):
+            data = self.get_entities_by_name('intern', conf_id, 'View')
+            views.append(('intern', data[0]['id']))
+
+        if view_arg in ('all', 'extern'):
+            data = self.get_entities_by_name('extern', conf_id, 'View')
+            views.append(('extern', data[0]['id']))
+
+        return views
+
+    def record_type_from_target(self, target):
+        '''Return type of record based on the target.
+        HostRecord if the target is an IP address,
+        AliasRecord if it is a domain
+        '''
+        try:
+            ipaddress.ip_address(target)
+            return 'HostRecord'
+        except ValueError:
+            return 'AliasRecord'
+
+    def get_record(self, view, domain):
+        '''Get record for domain in specified view.
+        '''
+
+        zones, host = self.parse_domain(domain)
+
+        # Navigate through zones
+        parent = view
+        for zone in zones:
+            data = self.get_entities_by_name(zone, parent, 'Zone')
+            if not data:
+                zone_path = ' → '.join(zones)
+                raise Exception(f'Zone {zone_path} could not be found.')
+            parent = data[0]['id']
+
+        # Get host
+        data = self.get_entities_by_name(host, parent, 'HostRecord') \
+            + self.get_entities_by_name(host, parent, 'AliasRecord')
+        for record in data:
+            properties = record['properties'].split('|')
+            return [prop for prop in filter(bool, properties)]
+
+    def set_record(self, view, domain, target):
+
+        record_type = self.record_type_from_target(target)
+
+        if record_type == 'HostRecord':
+            params = {'absoluteName': domain,
+                      'addresses': target,
+                      'ttl': -1,
+                      'viewId': view}
+            return self.__post('addHostRecord', params)
+
+        else:  # add aliias record
+            params = {'absoluteName': domain,
+                      'linkedRecordName': target,
+                      'ttl': -1,
+                      'viewId': view}
+            return self.__post('addAliasRecord', params)
+
+    def delete_record(self, view, domain):
+
+        zones, host = self.parse_domain(domain)
+
+        # Navigate through zones
+        parent = view
+        for zone in zones:
+            data = self.get_entities_by_name(zone, parent, 'Zone')
+            parent = data[0]['id']
+
+        # Get host
+        data = self.get_entities_by_name(host, parent, 'HostRecord') \
+            or self.get_entities_by_name(host, parent, 'AliasRecord')
+        if not data:
+            return
+
+        payload = {'objectId': data[0]['id']}
+        result = self.__delete('delete', payload)
+        return result
