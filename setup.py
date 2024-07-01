9e146616333345f9494a05dda156ac7e0f94f644
Lars Kiesow <lkiesow@uos.de>
2024-07-01 12:06:53 +0200

    Release 0.9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(filename):
 
 setup(
     name='proteuscmd',
-    version='0.8',
+    version='0.9',
     description=description_text,
     url='https://github.com/virtUOS/proteuscmd',
     author='Lars Kiesow',
